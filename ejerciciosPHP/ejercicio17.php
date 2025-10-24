38159259ef69b4adb51bf837cf94a0e509e988db
SrAllen14 <alvaro.allper.1@educa.jcyl.es>
2025-10-24 12:17:28 +0200

    Cambio en /ejerciciosPHP/ejercicio17.php: añadido foreach() y cambio de indice al iniciarlizar la matriz. Todo comentado.

--- ejerciciosPHP/ejercicio17.php
+++ ejerciciosPHP/ejercicio17.php
@@ -33,14 +33,16 @@
              * y 15 asientos por fila.
              */
         
+            $FILAS = 20;
+            $ASIENTOS = 15;
             // Declaramos las variables.
             $aNombres = ["Pepe", "Manuel", "Teresa", "Ainhoa", "Miguel"];
             $mTeatro;
             $bPosicionOcupada = false;
             
             // Inicializamos la matriz de 20X15 con " x " como valor por defecto.
-            for($i = 0; $i < 20; $i++){
-                for($j = 0; $j < 15; $j++){
+            for($i = 1; $i <= $FILAS; $i++){
+                for($j = 1; $j <= $ASIENTOS; $j++){
                     $mTeatro[$i][$j] = " x ";
                 }
             }
@@ -50,8 +52,8 @@
             // Situamos los nombres del array dentro de la matriz de manera aleatoria.
             // Controlamos que no se sobreescriban los nombres.
             while($i < 5){
-                $fila = random_int(0, 19);
-                $columna = random_int(0, 14);
+                $fila = random_int(1, 20);
+                $columna = random_int(1, 15);
                 if($mTeatro[$fila][$columna] === " x "){
                     $mTeatro[$fila][$columna] = $aNombres[$i];
                     $i++;
@@ -68,19 +70,44 @@
             
             // Mostramos la información de la matriz.
             echo "<table>";
-            for($i = 0; $i < 20; $i++){
+            for($i = 1; $i <= $FILAS; $i++){
                 echo "<tr>";
-                echo "<td class='filas'>Fila ".($i+1)."</td>";
-                for($j = 0; $j < 15; $j++){
+                echo "<td class='filas'>Fila ".($i)."</td>";
+                for($j = 1; $j <= $ASIENTOS; $j++){
                     if($mTeatro[$i][$j] === " x "){
-                        echo "<td class='libre'>F".($i+1)."-A".($j+1)."</td>";
+                        echo "<td class='libre'>F".($i)."-A".($j)."</td>";
                     } else{
                         echo "<td class='ocupado'>" . $mTeatro[$i][$j]. "</td>";
                     }
                 }
                 echo "</tr>";
             }
-            echo "</table>"
+            echo "</table>";
+            
+            echo "<br>";
+            
+            // Vamos a recorrer la matriz mediante un foreach();
+            echo "<h3>Tabla con foreach()</h3>";
+            
+            echo "<table>";
+            // En este caso estamos introduciendo en la variable $filas el número de la fila en la que estamos.
+            // En la variable $aAsientos tenemos el array de cada $fila.
+            foreach ($mTeatro as $filas => $aAsientos) {
+                echo "<tr>";
+                echo "<td class='filas'>Fila ".($filas)."</td>";
+                // Ahora mediante otro foreeach() inicializamos la variable $asiento con el numero de asiento.
+                // Y la variable $valor con el contenido de la posición $filas.
+                foreach ($aAsientos as $asiento => $valor) {
+                    if($valor === " x "){
+                        echo "<td class='libre'>F".($asiento)."-A".$filas."</td>";
+                    } else{
+                        echo "<td class='ocupado'>" . $valor. "</td>";
+                    }   
+                    
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
         ?>
     </body>
 </html>
