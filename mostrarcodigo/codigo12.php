ecfb8b0958f54d94bbefe6e4ae9e3dc81d71e171
SrAllen14 <alvaro.allper.1@educa.jcyl.es>
2025-10-28 13:45:51 +0100

    Modificados todos archivos del directorio /mostrarcodigo: añadidos estilos.

--- mostrarcodigo/codigo12.php
+++ mostrarcodigo/codigo12.php
@@ -1,11 +1,67 @@
 <!doctype html>
 <html lang="es">
     <head>
-        <title>Ejercicio 12</title>
+        <title>Código Ejercicio 12</title>
+        <style>
+            *{
+                margin: 0 auto;
+                padding: 0 auto;
+            }
+            body{
+                font-family: Arial, sans-serif;
+                background: #f4f6f9;
+                align-items: center;
+                text-align: center;
+            }
+
+            nav{
+                background-color: #456D96; 
+                color: white;
+            }
+
+            .ejercicio{
+                text-align: justify;
+                margin-top: 10px;
+                margin-bottom: 10px;
+
+                p{
+                    font-weight: bold;
+                }
+            }
+
+            footer{
+                margin: auto;
+                background-color: #456d96;
+                text-align: center;
+                align-content: center;
+                height: 50px;;
+                color: white;
+
+                & a{
+                   text-decoration: none; 
+                }
+            }
+        </style>
     </head>
     <body>
-        <?php
-            highlight_file("../codigoPHP/ejercicio12.php");
-        ?>
+        <nav>
+            <h2>DWES - Tema 3</h2>
+            <h2>Código Ejercicio 12</h2>
+        </nav>
+        <main>
+            <div class="ejercicio">
+                <?php 
+                    highlight_file("../codigoPHP/ejercicio12.php");
+                ?>
+            </div>
+        </main>
+        <footer>
+            <div>
+                <a href="../indexProyectoTema3.php">
+                Álvaro Allén Perlines
+                </a>
+                <time datetime="2025-10-28">28-10-2025</time>
+            </div>
+        </footer>
     </body>
 </html>
